843239616914213ee6891436a2a5137d9cd8f4e2
Chris H <pyuuch@outlook.com>
2016-12-24 14:17:39 -0600

    Pastebin distribution version

---
+++ plip-pastebin.lua
@@ -0,0 +1,9 @@
+print("plip is now downloading.")
+if (not http) then error("HTTP is disabled.") end
+local a = http.get("https://raw.githubusercontent.com/PyuuCH/CC-plip/master/plip.lua")
+if (not a) then error("Failed to connect to github.") end
+local b = a:readAll()
+local c = fs.open("plip","w")
+c:write(b)
+c:close()
+print("Done! You may now run plip as normal.")
